d8f08bb3bdf2f83ef819da6ce3c3e7101facd5e7
‘me’ <Mari-Holiday@users.noreply.github.com>
2018-10-02 21:25:53 +0900

    add kuma

---
+++ Assets/Script/ColorChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    //アタッチのGameObjectから取得
+    SpriteRenderer kumaSprite;
+
+    //変更対象のSpriteを保持するGameObjectを取得
+    GameObject changeSprite;
+
+    void Start()
+    {
+        kumaSprite = gameObject.getComponent();
+        Debug.Log(kumaSprite.color);
+    }
+
+    void Update()
+    {
+        
+    }
+}
