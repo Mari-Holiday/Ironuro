c81f3a2a242d0ad0b981f287ad0b87d763e1f797
‘me’ <Mari-Holiday@users.noreply.github.com>
2018-10-03 15:30:42 +0900

    くまとお花が被ってる箇所をタップしたらお花の色が変わる機能を作成

---
+++ Assets/Script/ColorChange.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ColorChange : MonoBehaviour
+{
+    List<GameObject> clickedGameObject;
+
+    GameObject tapKuma;
+    GameObject tapOhana;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedGameObject = new List<GameObject>();
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hit2d = Physics2D.RaycastAll((Vector2)ray.origin, (Vector2)ray.direction, 10);
+
+            if (hit2d[0])
+            {
+                for (int i = 0; i < hit2d.Length; i++)
+                {
+                    if (hit2d[i].transform.gameObject == gameObject)
+                    {
+                        tapKuma = hit2d[i].transform.gameObject;
+
+                        Debug.Log("わたしはくまちゃん" + hit2d[i].transform.gameObject.name + i);
+
+                        for (int j = 0; j < hit2d.Length; j++)
+                        {
+                            //Rayが当たったオブジェクトのtagがOhanaだったら
+                            if (hit2d[j].collider.tag == "ohana")
+                            {
+                                tapOhana = hit2d[j].transform.gameObject;
+
+                                Debug.Log("Rayがohanaに当たった");
+                                tapOhana.GetComponent<SpriteRenderer>().color
+                                    = tapKuma.GetComponent<SpriteRenderer>().color;
+
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
--- Assets/Script/KumaScript.cs
+++ Assets/Script/KumaScript.cs
@@ -8,36 +8,49 @@ public class KumaScript : MonoBehaviour
 {
     //アタッチのGameObjectから取得
     SpriteRenderer kumaSprite;
-    TapGesture tapGesture;
 
-    //変更対象のSpriteを保持するGameObjectを取得
-    GameObject changeSprite;
+    //public TapGesture tapGesture;
 
+    //変更対象のSpriteを保持するGameObject(取得予定)
+    public SpriteRenderer changeSprite;
 
 
     void Start()
     {
         kumaSprite = gameObject.GetComponent<SpriteRenderer>();
-        Debug.Log(kumaSprite.color);
-        tapGesture = gameObject.GetComponent<TapGesture>();
+        //Debug.Log(kumaSprite.color);
     }
 
     void Update()
     {
+        //実機用
+        if(Input.touchCount > 0){
+            Debug.Log("inputtouchsareta");
+        }
 
+        //検証用
+        if(Input.GetMouseButtonDown(0)){
+
+            
+            //changeSprite.color = kumaSprite.color;
+        }
     }
 
-    private void OnEnable()
-    {
-        tapGesture.Tapped += OnTapped;
-    }
 
-    private void OnDisable()
-    {
-        tapGesture.Tapped -= OnTapped;
-    }
 
-    private void OnTapped(object sender,EventArgs e){
-        Debug.Log("");
-    }
+    //＝＝＝＝＝実機タップジェスチャー用
+    // private void OnEnable()
+    // {
+    //     tapGesture.Tapped += OnTapped;
+    // }
+
+    // private void OnDisable()
+    // {
+    //     tapGesture.Tapped -= OnTapped;
+    // }
+
+    // private void OnTapped(object sender,EventArgs e){
+    //     Debug.Log("touchsareta");
+    //     changeSprite.color = kumaSprite.color;
+    // }
 }
