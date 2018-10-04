6588d8c2a6ba4f5362269e65c56eeaa807dee271
‘me’ <Mari-Holiday@users.noreply.github.com>
2018-10-04 21:55:18 +0900

    add Dotween ,  create Title Scene

---
+++ Assets/Script/TitleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TitleScript : MonoBehaviour
+{
+    Quaternion rotateNormal;
+
+    float timeElapsed;
+
+    void Start()
+    {
+        rotateNormal = gameObject.GetComponent<Transform>().rotation;
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= 2.0f)
+        {
+            gameObject.transform.DOShakeRotation(1f, 15, 5, 10);
+            gameObject.transform.rotation = rotateNormal;
+
+            timeElapsed = 0.0f;
+        }
+    }
+}
