b95b0d71e3fdb525d84beac5bf275b5937490176
‘me’ <Mari-Holiday@users.noreply.github.com>
2018-10-04 16:14:59 +0900

    （不足分追加）

---
+++ Assets/Script/ColorClass.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorClass : MonoBehaviour
+{
+    public GameObject kumaPenWhite;
+    public GameObject kumaPenBlack;
+    public GameObject kumaPenRed;
+    public GameObject kumaPenBlue;
+    public GameObject kumaPenYellow;
+    public GameObject kumaPenGreen;
+
+    private static Color penWhite;
+    private static Color penBlack;
+    private static Color penRed;
+    private static Color penBlue;
+    private static Color penYellow;
+    private static Color penGreen;
+
+    void Start()
+    {
+        //ヒエラルキー上のサンプルくまから6色の取得、クラスフィールドに保持
+        penWhite = kumaPenWhite.GetComponent<SpriteRenderer>().color;
+        penBlack = kumaPenBlack.GetComponent<SpriteRenderer>().color;
+        penRed = kumaPenRed.GetComponent<SpriteRenderer>().color;
+        penBlue = kumaPenBlue.GetComponent<SpriteRenderer>().color;
+        penYellow = kumaPenYellow.GetComponent<SpriteRenderer>().color;
+        penGreen = kumaPenGreen.GetComponent<SpriteRenderer>().color;
+    }
+
+    void Update()
+    {
+
+    }
+
+    //引数の番号で各色を返すメソッド
+    public static Color chosePenColor(int colorNum)
+    {
+        if (colorNum == 0) { return penWhite; }
+        if (colorNum == 1) { return penBlack; }
+        if (colorNum == 2) { return penRed; }
+        if (colorNum == 3) { return penBlue; }
+        if (colorNum == 4) { return penYellow; }
+        if (colorNum == 5) { return penGreen; }
+        else { return penWhite; }
+    }
+
+    //各色ゲットメソッド
+    public static Color getPenWhite()
+    {
+        return penWhite;
+    }
+
+    public static Color getPenBlack()
+    {
+        return penBlack;
+    }
+    public static Color getPenRed()
+    {
+        return penRed;
+    }
+    public static Color getPenBlue()
+    {
+        return penBlue;
+    }
+    public static Color getPenYellow()
+    {
+        return penYellow;
+    }
+    public static Color getPenGreen()
+    {
+        return penGreen;
+    }
+
+}
